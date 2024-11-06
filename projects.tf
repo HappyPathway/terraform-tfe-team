6a4913e61ce273600f8d8ee25df38996189c7859
Dave Arnold <dave@roknsound.com>
2024-11-06 11:04:36 -0800

    Refactor project access in projects.tf to use correct project ID

--- projects.tf
+++ projects.tf
@@ -9,7 +9,7 @@ data "tfe_project" "projects" {
 resource "tfe_team_project_access" "team_project_access" {
   for_each   = tomap({ for project in var.tfe_projects : project.name => project.access_level }) # Map project names to their access levels
   team_id    = local.tfe_team.id                                                                 # Lookup the team ID based on the project name
-  project_id = lookup(data.tfe_project.project, each.key).id                                     # Set the project ID
+  project_id = lookup(data.tfe_project.projects, each.key).id                                    # Set the project ID
   access     = each.value                                                                        # Set the access level for the team
 }
 
