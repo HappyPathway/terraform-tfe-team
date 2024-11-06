8886ef251f666feb8ab8092e7509ce002baa97ac
Dave Arnold <dave@roknsound.com>
2024-11-05 16:38:31 -0800

    Refactor variables.tf to add a new variable for the team token storage path in Vault

--- variables.tf
+++ variables.tf
@@ -8,15 +8,16 @@ variable "organization" {
   type        = string
 }
 
+variable "vault_secret_path" {
+  description = "The path in Vault where the team token will be stored"
+  type        = string
+}
+
+
 variable "team_members" {
   description = "A list of usernames to add to the team"
   type        = list(string)
   default     = []
-}
-
-variable "vault_secret_path" {
-  description = "The path in Vault where the team token will be stored"
-  type        = string
 }
 
 variable "tfe_workspaces" {
