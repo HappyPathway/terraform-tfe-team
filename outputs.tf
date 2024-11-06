69de7938c1df1404f3f9be04ec8f6d9a09390666
Dave Arnold <dave@roknsound.com>
2024-11-06 11:03:50 -0800

    Refactor outputs.tf to remove unused project_id output

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "team_id" {
   description = "The ID of the created team"
   value       = local.tfe_team.id
 }
-
-output "project_id" {
-  description = "The ID of the created project"
-  value       = tfe_project.project.id
-}
